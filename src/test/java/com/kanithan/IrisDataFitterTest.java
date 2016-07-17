7c9720d9ef5555addb92f63aaf41ded5e36beed1
venkatkrish <venkataramanan-k@hcl.com>
2016-07-17 16:26:59 -0400

    Newly added files

---
+++ src/test/java/com/kanithan/IrisDataFitterTest.java
@@ -0,0 +1,53 @@
+package com.kanithan;
+
+import java.io.Serializable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kanithan.classifier.ModelLegoFitter;
+
+/**
+ * Unit test for simple App.
+ */
+public class IrisDataFitterTest implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private ModelLegoFitter modelLegoFitter;
+	
+
+	@Before
+	public void setUp() {
+		
+		String fileName = this.getClass().getClassLoader().getResource("iris.csv").toString();
+		
+		System.out.println("File input : "+ fileName);
+		
+		String[] columns = {"sepallength", "sepalwidth", "petallength", "petalwidth", "class"};
+		
+		modelLegoFitter = new ModelLegoFitter(fileName, 0.8f, 0.2f, columns);
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println("Tear down");
+	}
+
+	@Test
+	public void testEvaluate() {
+
+//		double accuracy = decisionTree.evaluateAndPredict();
+		
+		double dtAccuracy = modelLegoFitter.evaluateAndPredict("DecisionTree");
+
+		double rfAccuracy = modelLegoFitter.evaluateAndPredict("RandomForest");
+
+		System.out.println("Precision Accuracy value of Decision Tree : " + dtAccuracy+", Random Forest:"+ rfAccuracy);
+	}
+
+}
